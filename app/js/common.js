c4fcbcaea597040fd369baff06ede06aa5805f66
avnicolaevich <avnicolaevich@gmail.com>
2017-04-30 00:21:22 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    // $(":checkbox").labelauty();
+
+
+    // $('.header__sign').on('click', function (e) {
+    //     e.preventDefault();
+    //     $(this).parent().addClass('active');
+    //     $(this).parent().siblings().removeClass('active');
+    //     target = $(this).attr('href');
+    //     $('.header__content > div').not(target).hide();
+    //     $(target).fadeIn(600);
+    // });
+
+
+    // $('#selectState').scombobox({
+    //     filterDelay: 300,
+    //     highlight: true,
+    //     filterIgnoreCase: true,
+    //     fillOnTab: true,
+    //     showDropDown: true,
+    //     placeholder: ' '
+    // });
+
+    // $('.header__link').click(function() {
+    //     var checkElement = $(this).next();
+    //     checkElement.stop().animate({'height':'toggle'}, 0.1, 'linear');
+    //     if((checkElement.is('form')) && (checkElement.is(':visible'))) {
+    //         return false;
+    //     } else if ((checkElement.is('form')) && (checkElement.is(':hidden'))) {
+    //         return true;
+    //     }
+    // });
+
+    // $(document).click( function(event){
+    //     if( $(event.target).closest("form").length )
+    //         return;
+    //     $(".registration").fadeOut("0.1");
+    //     event.stopPropagation();
+    // });
+});
+
+
